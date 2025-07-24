53c967dca7d703f51ba0a04e574c140e7d80793e
Leehyewon75 <kb202322@gmail.com>
2025-07-24 09:51:51 +0900

    Update memory_game.py

--- memory_game.py
+++ memory_game.py
@@ -3,15 +3,16 @@ import numpy as np
 import random
 import copy
 
-st.set_page_config(page_title="HTML 스도쿠", layout="centered")
-st.title("🧩 HTML 기반 스도쿠")
+st.set_page_config(page_title="스도쿠", layout="centered")
+st.title("🧩 종이 느낌 스도쿠")
 
-# --- 스도쿠 생성 함수 ---
+# --- 스도쿠 생성 ---
 def is_valid(board, row, col, num):
-    if num in board[row]: return False
-    if num in board[:, col]: return False
+    if num in board[row] or num in board[:, col]:
+        return False
     sr, sc = 3 * (row // 3), 3 * (col // 3)
-    if num in board[sr:sr+3, sc:sc+3]: return False
+    if num in board[sr:sr+3, sc:sc+3]:
+        return False
     return True
 
 def solve(board):
@@ -48,7 +49,7 @@ if "sudoku" not in st.session_state:
     st.session_state.user_input = [["" for _ in range(9)] for _ in range(9)]
     st.session_state.checked = False
 
-# --- 새 퍼즐 버튼 ---
+# --- 새 게임 ---
 if st.button("🔁 새 퍼즐 생성"):
     puzzle, solution = generate_sudoku(clues=35)
     st.session_state.sudoku = puzzle
@@ -57,61 +58,72 @@ if st.button("🔁 새 퍼즐 생성"):
     st.session_state.checked = False
     st.rerun()
 
-# --- 스타일 ---
-def get_cell_style(i, j, is_fixed, is_wrong=False):
-    style = "width:40px; height:40px; text-align:center; font-size:20px;"
-    style += "border:1px solid #999;"
-    if i % 3 == 0:
-        style += "border-top: 2px solid black;"
-    if j % 3 == 0:
-        style += "border-left: 2px solid black;"
-    if i == 8:
-        style += "border-bottom: 2px solid black;"
-    if j == 8:
-        style += "border-right: 2px solid black;"
-    if is_fixed:
-        style += "background-color:#eee;"
-    elif is_wrong:
-        style += "background-color:#ffcccc;"
-    elif not is_fixed:
-        style += "background-color:#fff;"
-    return style
+# --- 테이블 렌더링 ---
+st.write("👇 숫자(1~9)를 직접 입력해보세요")
 
-# --- 렌더링 ---
-st.write("👇 빈 칸에 숫자(1~9)를 입력하세요")
+table_html = """
+<style>
+table {
+    border-collapse: collapse;
+    margin-top: 10px;
+}
+td {
+    width: 45px;
+    height: 45px;
+    text-align: center;
+    vertical-align: middle;
+    font-size: 20px;
+    font-family: monospace;
+    border: 1px solid black;
+}
+td.fixed {
+    background-color: #ddd;
+}
+td.incorrect {
+    background-color: #ffdddd;
+}
+td.correct {
+    background-color: #ddffdd;
+}
+td:nth-child(3), td:nth-child(6) {
+    border-right: 3px solid black;
+}
+tr:nth-child(3) td, tr:nth-child(6) td {
+    border-bottom: 3px solid black;
+}
+</style>
+<table>
+"""
 
-table_html = "<table style='border-collapse: collapse;'>"
 for i in range(9):
     table_html += "<tr>"
     for j in range(9):
-        fixed_val = st.session_state.sudoku[i][j]
-        solution_val = st.session_state.solution[i][j]
+        val = st.session_state.sudoku[i][j]
         key = f"cell_{i}_{j}"
-
-        if fixed_val != 0:
-            style = get_cell_style(i, j, is_fixed=True)
-            table_html += f"<td style='{style}'>{fixed_val}</td>"
+        if val != 0:
+            table_html += f"<td class='fixed'>{val}</td>"
         else:
             user_val = st.session_state.user_input[i][j]
             input_val = st.text_input("", value=user_val, key=key, max_chars=1, label_visibility="collapsed")
 
-            # 숫자 필터
+            # 유효한 값만 반영
             if input_val in "123456789":
                 st.session_state.user_input[i][j] = input_val
             elif input_val == "":
                 st.session_state.user_input[i][j] = ""
             else:
-                input_val = ""
+                st.session_state.user_input[i][j] = ""
 
-            # 오답일 경우 스타일 다르게
-            is_wrong = False
+            # 정답 확인 시 표시
+            css_class = ""
             if st.session_state.checked:
-                if input_val != str(solution_val):
-                    is_wrong = True
+                correct_val = str(st.session_state.solution[i][j])
+                if input_val == correct_val:
+                    css_class = "correct"
+                else:
+                    css_class = "incorrect"
 
-            style = get_cell_style(i, j, is_fixed=False, is_wrong=is_wrong)
-            table_html += f"<td style='{style}'>{input_val}</td>"
-
+            table_html += f"<td class='{css_class}'>{input_val}</td>"
     table_html += "</tr>"
 table_html += "</table>"
 
@@ -124,10 +136,10 @@ if st.button("✅ 정답 확인"):
     for i in range(9):
         for j in range(9):
             if st.session_state.sudoku[i][j] == 0:
-                user_val = st.session_state.user_input[i][j]
-                if user_val != str(st.session_state.solution[i][j]):
+                u = st.session_state.user_input[i][j]
+                if u != str(st.session_state.solution[i][j]):
                     incorrect = True
     if incorrect:
         st.error("❌ 틀린 칸이 있어요. 다시 확인해보세요!")
     else:
-        st.success("🎉 정답입니다! 잘했어요.")
+        st.success("🎉 정답입니다! 퍼즐을 완료했어요.")
